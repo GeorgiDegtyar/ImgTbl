f0fcca003a94557af415f2442bbbca9f43e91c93
GEO <dghteng@gmail.com>
2015-08-16 21:50:58 +0300

    Initial Commit

---
+++ ImgTbl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImgTbl
+//
+//  Created by GEO on 16.08.15.
+//  Copyright (c) 2015 GEO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImgTbl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImgTbl
+//
+//  Created by GEO on 16.08.15.
+//  Copyright (c) 2015 GEO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
